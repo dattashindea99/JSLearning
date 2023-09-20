9007513838a80166d75c840de0af4ce519857d0d
Datta Manik Shinde <dattashindea99@gmail.com>
2023-09-20 19:00:35 +0530

    variable Assignment
    variable Assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,23 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Datta Shinde";
+console.log(myFullName);
+myFullName="Datta Manik Shinde";
+console.log("updated with middle name:",myFullName);
+var mylove = "I love only JS";
+console.log(mylove);
+var firstName="Datta";
+console.log("first Name :",firstName);
+var lastName="Shinde";
+console.log("Last Name :",lastName);
+var age="24";
+console.log("My Age :",age);
+var address="Pune Maharashtra "
+console.log("My Address :",address);
+var pincode="411011";
+console.log("Pin Code :",pincode);
+var Gender="Male";
+console.log("Gender :",Gender);
+var isMarried="NO";
+console.log("isMarried :",isMarried);
+
