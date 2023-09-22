dfa496612a4a475be68b5032ca425ec376f96aa6
Datta Manik Shinde <dattashindea99@gmail.com>
2023-09-23 00:22:32 +0530

    Created 02_variableAssign.js
    Created 02_variableAssign.js

---
+++ 02_variableBasicAssig.js
@@ -0,0 +1,14 @@
+
+console.log("=======================Assignment 1 ===========================");
+console.log("=======================Part 1 =================================");
+var houseNo = 9826;
+console.log("House Number :",houseNo,"  Data Type :",typeof houseNo);
+var Name = "Datta";
+console.log("Name :",Name,"         Data Type :",typeof Name);
+var isStudent = true;
+
+
+console.log("is Student :",isStudent,"    Data Type",typeof isStudent);
+console.log("=========================Part 2 ==================================");
+console.log(" Null : It represents an empty variable . It means variable has no Value. ");
+console.log("Undefine : It means variable has been declared but not yet initialized.");
