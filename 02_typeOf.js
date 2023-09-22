1d545e40f80d8e7de1262c8029f7d5d53203e32c
Datta Manik Shinde <dattashindea99@gmail.com>
2023-09-23 00:23:20 +0530

    Created 02_type of JS
    Created 02_variableAssign.js

---
+++ 02_typeOf.js
@@ -0,0 +1,30 @@
+console.log("=======================Assignment 2 ===========================");
+console.log("=======================Part 1 =================================");
+
+
+var bankName;
+console.log("bankName before initialization :",bankName,  "     Data Type :",typeof bankName);
+
+bankName = "SBI";
+console.log("bankName After initialization :",bankName,"            Data Type :",typeof bankName);
+
+
+console.log("=======================Part 2 =================================");
+
+
+var marks = '90%';
+console.log("Marks :",marks, " Data Type :" , typeof marks);
+
+console.log("=======================Part 3 =================================");
+var isWorking;
+console.log("Before initialization :",isWorking, "              Data Type :", typeof isWorking);
+isWorking=true;
+console.log("After initialization :",isWorking, "                    Data Type :" ,typeof isWorking);
+
+
+console.log("=======================Part 4 =================================");
+
+var totalCount = "Hundred and Seven";
+console.log("totalCount :",totalCount, "         Data Type :",typeof totalCount);
+totalCount=107;
+console.log("totalCount :",totalCount, "                       Data Type :",typeof totalCount);
