8013702a865d06da9a5df518c6fcf39b7d4e4315
Datta Manik Shinde <dattashindea99@gmail.com>
2023-09-26 06:19:15 +0530

    created 03_funAssign02.js
    created 03_funAssign02.js

---
+++ 03_funAssig02.js
@@ -0,0 +1,7 @@
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("Bank Name :",bankName,"    Account Number",accountNum,"     Location :",location,"    PIN Code :",pinCode);
+
+}
+bankDetails("CITI Bank",6656556565662,'Pune',431202)
+bankDetails("Axis Bank",785648955,'Mumbai',441202)
+bankDetails("HDFC Bank",89745856656,'Pune',631202)
