e0ca11c83073f7259b23617d3916afc30cb12ce5
Datta Manik Shinde <dattashindea99@gmail.com>
2023-09-26 06:20:05 +0530

    03_stringBasicAssig01.js
    03_stringBasicAssig01.js

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,17 @@
+function stringBasics(){
+    console.log('My Dream Company Name is "Gooogle"');
+
+}
+stringBasics();
+var hobby1="Cricket";
+var hobby2 = "Playing Games";
+var hobby3 = "Music";
+console.log("My hobbies are",hobby1,hobby2,hobby3);
+var lengthOfHobby1=hobby1.length;
+console.log("NO of Character in hobby 1 are",lengthOfHobby1); 
+var lengthOfHobby2 = hobby2.length
+console.log("NO of Character in hobby 2 are",lengthOfHobby2);
+var lengthOfHobby3 = hobby3.length;
+console.log("NO of Character in hobby 3 are",lengthOfHobby3);
+var result = lengthOfHobby1 + lengthOfHobby2 + lengthOfHobby3;
+console.log("Total No Of Character ",result);
