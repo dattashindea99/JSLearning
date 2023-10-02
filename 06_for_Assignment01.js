48a4147e690da771ec5d8b2a65d8ad5c1201afd0
Datta Manik Shinde <dattashindea99@gmail.com>
2023-10-02 20:15:57 +0530

    print number 5 to 15
    print number 5 to 15

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,59 @@
+console.log(`====================print number 5 to 15 ======================`);
+
+var result="";
+for (let index = 5; index <=15; index++) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+console.log(`====================print number 50 to 40 by decrementing ======================`);
+ var result = "";
+ for (let index = 50; index >=40; index--) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+console.log(`====================To find first 15 odd numbers======================`);
+ var result = "";
+ for (let index = 1; index <=30; index=index+2) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+console.log(`====================To find first 10 even numbers======================`);
+ var result = "";
+ for (let index = 2; index <=10; index=index+2) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+console.log(`====================To Print Table of 5======================`);
+ var result = "";
+ for (let index = 5; index <=50; index=index+5) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+
+console.log(`====================To Print Table of 10======================`);
+ var result = "";
+ for (let index = 10; index <=100; index=index+10) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+console.log(`====================To Print Table of 10======================`);
+ var result = "";
+ for (let index = 100; index >=10; index=index-10) {
+    result=result + index +  "   ";
+    }
+console.log(result);
+
+
+
+
+
+
+
+
+ 
